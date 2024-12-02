688ce4d2c7b878e198c976796328d2a9e9fcd654
bror <reachingbror@gmail.com>
2024-12-02 16:54:24 +0100

    adding the input in a seperate file that i read from in my script. Also got the right output fron my function

--- day_01.py
+++ day_01.py
@@ -1,33 +1,20 @@
-left_list = [88276,66898,58877,69396,53434,69841]
-right_list = [66757,13714,87487,12997,62485,67231]
-
+left_list = []
+right_list = []
+with open('input_day01.txt', 'r') as file:
+    for line in file:
+        numbers = line.split()
+        left_num = int(numbers[0])
+        right_num = int(numbers[1])
+        left_list.append(left_num)
+        right_list.append(right_num)
 
 left_list_sorted = sorted(left_list)
 right_list_sorted = sorted(right_list)
 
 length_lists = len(left_list)
-diff_list = 0 # här håller jag koll på "avståndet" för varje nummer
+diff_list = 0 # keep track of the diff
 
 for i in range (length_lists):
-    print(right_list_sorted[i] - left_list_sorted[i])
     diff_list += abs(right_list_sorted[i] - left_list_sorted[i])
-    print(diff_list)
 
-print(left_list_sorted)
-print(right_list_sorted)
-
-
-#test from input
-#left_list = [3,4,2,1,3,3]
-#right_list = [4,3,5,3,9,3]
-
-"""
-88276   66757
-66898   13714
-58877   87487
-69396   12997
-53434   62485
-69841   67231
-51135   99904
-75350   18379
-"""
+print(diff_list)
