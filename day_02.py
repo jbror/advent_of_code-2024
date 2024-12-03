5ddb9cb622ada9a3d0d641cb24b7c1d4c1d1dd83
bror <reachingbror@gmail.com>
2024-12-03 19:59:01 +0100

    still working on the logic.. Draft and BUP for now

--- day_02.py
+++ day_02.py
@@ -9,34 +9,25 @@ data_input = [
 
 
 
-"""
-rules
 
-The levels are either all increasing or all decreasing.
-Any two adjacent levels differ by at least one and at most three.
-
-5 levels (row)
-6 reports (columns)
-
-example data
-
-7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9
+report1 = [7, 6, 4, 2, 1]
+kaka = report1[1] and report1[2] > report1[0]
+kaka2 = report1[1] and report1[2] < report1[0]
+#check = 0
 
 
-for use later
-
-with open('input_day01.txt', 'r') as file:
-    for line in file:
-        numbers = line.split()
-        left_num = int(numbers[0])
-        right_num = int(numbers[1])
-        left_list.append(left_num)
-        right_list.append(right_num)
+# ökar eller minskar?
+#är report1[1] och report1[2] högre elller lägre än report[0]?
 
 
-"""
+
+
+
+
+#rules
+
+#The levels are either all increasing or all decreasing.
+#Any two adjacent levels differ by at least one and at most three.
+
+#5 levels (row)
+#6 reports (columns)
