b5cc29b76856a5ea1388f8993103ccc4d6ecce6c
bror <reachingbror@gmail.com>
2024-12-08 00:13:15 +0100

    adding testfile for my datainput and also added logic for working with the input in my script from the file. Still work in progress

--- day_02.py
+++ day_02.py
@@ -7,6 +7,28 @@ data_input = [
 [1,3,6,7,9]
 ]
 
+report_list2 = []
+report_list = []
+with open('input_test_day02.txt', 'r') as file:
+    for line in file:
+        numbers = line.split()
+        for n in numbers:
+            int_n = int(n)
+            report_list2.append(int_n)
+            print(report_list2)
+        report_list.append(report_list2)
+        report_list2 = []
+
+            #print(int_n)
+            #print(type(int_n))
+        #print(numbers)
+        #level_num = int(numbers)
+        #report_list.append(level_num)
+        #print(report_list)
+
+print(report_list)
+data_input = report_list
+
 for l in data_input:
     my_list = l
     is_increasing = True
